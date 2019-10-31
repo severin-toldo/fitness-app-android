1870b65882f8eba987bb7ff14b574961f26ea99b
stoldo <stoldo@runmyaccounts.com>
2019-10-31 10:12:37 +0100

    project on working state

---
+++ app/src/main/java/com/stoldo/fitness_app_android/MainActivity.java
@@ -0,0 +1,21 @@
+package com.stoldo.fitness_app_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.stoldo.fitness_app_android.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/com/stoldo/fitness_app_android/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.stoldo.fitness_app_android.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
