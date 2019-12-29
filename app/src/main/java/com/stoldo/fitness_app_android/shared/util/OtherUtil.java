c9fe172ea1206de810d7433d843b77a452de16a9
stoldo <stoldo@runmyaccounts.com>
2019-12-29 11:36:56 +0100

    removed redunant methods

--- app/src/main/java/com/stoldo/fitness_app_android/shared/util/OtherUtil.java
+++ app/src/main/java/com/stoldo/fitness_app_android/shared/util/OtherUtil.java
@@ -5,39 +5,14 @@ import android.content.res.Resources;
 import android.util.Log;
 import android.util.TypedValue;
 
-import com.stoldo.fitness_app_android.model.interfaces.Subscriber;
 import com.stoldo.fitness_app_android.service.SingletonService;
 
-import java.util.Arrays;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 public class OtherUtil {
-    // TODO replace with model
-    public static Map<String, Object> getEditMenuEventMap(String actionValue, Boolean editModeValue) {
-        return getEventMap(Arrays.asList("action", "editMode"), Arrays.asList(actionValue, editModeValue));
-    }
-
-    // TODO replace with model
-    public static Map<String, Object> getTimerEventMap(Subscriber subscriberValue, Integer secondsValue) {
-        return getEventMap(Arrays.asList("subscriber", "seconds"), Arrays.asList(subscriberValue, secondsValue));
-    }
-
-    // TODO replace with model
-    public static Map<String, Object> getEventMap(List<String> keys, List<Object> values) {
-        Map<String, Object> map = new HashMap<>();
-        for (int i = 0; i < keys.size(); i++) {
-            map.put(keys.get(i), values.get(i));
-        }
-
-        return map;
-    }
-
     public static int convertDpToPixel(int dp, Context context){
         Resources r = context.getResources();
         return (int) TypedValue.applyDimension(
