2557b717515dc64ef7d772586f05f3eee88e70b7
KingStef2002 <stefano02.itri@gmail.com>
2020-01-19 16:55:12 +0100

    added function for multiple languages

--- app/src/main/java/com/stoldo/fitness_app_android/activities/SettingsActivity.java
+++ app/src/main/java/com/stoldo/fitness_app_android/activities/SettingsActivity.java
@@ -55,7 +55,7 @@ public class SettingsActivity extends PreferenceActivity {
         LocaleManager.setNewLocale(this, language);
         this.finish();
         Intent intent = mContext.getIntent();
-        startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
+        startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_ANIMATION));
     }
 
     @Override
